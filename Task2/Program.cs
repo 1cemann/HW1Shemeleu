29a4251ae39176cce3b34b0dcca2d3aa6fb54f95
Mad <ned_91@mail.ru>
2022-06-07 01:14:02 +0300

    add Task2 Finaly and add gitignore file

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+if(value1>value2)
+{
+    Console.Write("Число ");
+    Console.Write(value1);
+    Console.Write(" Больше чем число ");
+    Console.Write(value2);
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(value1);
+    Console.Write(" Меньше чем число ");
+    Console.Write(value2);
+}
